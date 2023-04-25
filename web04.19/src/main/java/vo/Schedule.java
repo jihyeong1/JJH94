22bcd458ae30eaa654a00ca2d0055fbf20800040
jihyeong1 <wlgud059@naver.com>
2023-04-25 11:23:29 +0900

    스케줄 클래스

---
+++ web04.19/src/main/java/vo/Schedule.java
@@ -0,0 +1,12 @@
+package vo;
+
+public class Schedule {
+	public int ScheduleNo;
+	public String ScheduleDate;
+	public String ScheduleTime;
+	public String ScheduleMemo;
+	public String ScheduleColor;
+	public String createdate;
+	public String updatedate;
+	public String SchedulePw;
+}
